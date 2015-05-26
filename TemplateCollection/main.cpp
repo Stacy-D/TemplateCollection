b00e0944ad345bc9e7eec0b2885ec74321899b5d
Stacy <stace1995D@gmail.com>
2015-05-26 17:38:35 +0300

    Initial Commit

---
+++ TemplateCollection/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TemplateCollection
+//
+//  Created by Anastasia on 5/26/15.
+//  Copyright (c) 2015 Anastasia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
